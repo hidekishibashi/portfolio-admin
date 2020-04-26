3cb68961412fff3720b310af0ceaad062c939b17
hidekishibashi <dev.hidekishibashi@gmail.com>
2020-04-26 19:51:02 +0900

    java file format

--- src/main/java/com/seattleacademy/team20/config/DBConfig.java
+++ src/main/java/com/seattleacademy/team20/config/DBConfig.java
@@ -10,26 +10,30 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
- @Configuration
- @PropertySource({"classpath:/jdbc.properties"})
+
+@Configuration
+@PropertySource({ "classpath:/jdbc.properties" })
 public class DBConfig {
-    @Autowired
-    private Environment environment;
-    @Bean
-    public DataSource datasource() {
-        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
-        dataSource.setUrl(environment.getProperty("jdbc.url"));
-        dataSource.setUsername(environment.getProperty("jdbc.username"));
-        dataSource.setPassword(environment.getProperty("jdbc.password"));
-        return dataSource;
-    }
-    @Bean
-    public DataSourceTransactionManager transactionManager() {
-      return new DataSourceTransactionManager(datasource());
-    }
-    @Bean
-    public JdbcTemplate jdbcTemplate() {
-      return new JdbcTemplate(datasource());
-    }
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	public DataSource datasource() {
+		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(environment.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(environment.getProperty("jdbc.url"));
+		dataSource.setUsername(environment.getProperty("jdbc.username"));
+		dataSource.setPassword(environment.getProperty("jdbc.password"));
+		return dataSource;
+	}
+
+	@Bean
+	public DataSourceTransactionManager transactionManager() {
+		return new DataSourceTransactionManager(datasource());
+	}
+
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(datasource());
+	}
 }
