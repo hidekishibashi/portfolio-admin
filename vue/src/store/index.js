6894bf334a39087254e5fe02206900f54f068008
hidekishibashi <dev.hidekishibashi@gmail.com>
2020-04-30 01:19:16 +0900

    add : api connect

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -1,11 +1,40 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
+const config = {
+  headers: {
+    "Access-Control-Allow-Origin": "*"
+  }
+};
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    skillCategories: [],
+    skills: []
+  },
+  mutations: {
+    setSkillCategories(state, payload) {
+      state.skillCategories = payload.skillCategories;
+    },
+    setSkills(state, payload) {
+      state.skills = payload.skills;
+    }
+  },
+  actions: {
+    fetchSkillInfos({ commit }) {
+      const url = "/api/skills";
+      axios.get(url + "/categories", config).then(res => {
+        const skillCategories = res.data;
+        commit("setSkillCategories", { skillCategories });
+      });
+      axios.get(url, config).then(res => {
+        const skills = res.data;
+        commit("setSkills", { skills });
+      });
+    }
+  },
   modules: {}
 });
